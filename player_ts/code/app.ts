be2d58c0af06b4cbb4bfac524341afbaafe0843a
= <=>
2023-03-10 10:04:42 -0300

    feat: nova estrutura da lista

--- player_ts/code/app.ts
+++ player_ts/code/app.ts
@@ -1,6 +1,49 @@
-class Player{
-    constructor( public name: string, public height: number, public weight: number, public age: number){}
+class Player {
+  constructor(
+    public name: string,
+    public height: number,
+    public weight: number,
+    public age: number
+  ) {}
+
+  get nome() {
+    return this.name;
+  }
+
+  get altura() {
+    return this.height;
+  }
+
+  get peso() {
+    return this.weight;
+  }
+
+  get idade() {
+    return this.age;
+  }
+
+}
+const player = new Player("John", 1.8, 70, 18);
+
+function addTr() {
+  let nomeTd = document.createElement("td");
+  let alturaTd = document.createElement("td");
+  let pesoTd = document.createElement("td");
+  let idadeTd = document.createElement("td");
+
+  
+  nomeTd.textContent = player.nome;
+  alturaTd.textContent = player.altura.toString();
+  pesoTd.textContent = player.peso.toString();
+  idadeTd.textContent = player.idade.toString();
+
+  let linha = document.createElement("tr");
+  linha.append(nomeTd, alturaTd, pesoTd, idadeTd);
+
+  document.querySelector(".tabela")?.appendChild(linha);
 }
 
- let player1 = new Player('Lázaro', 1.70, 67, 18);
- console.log(player1) 
+const button = document.querySelector("#button-add");
+button?.addEventListener("click", addTr);
+
+
--- player_ts/prod/app.js
+++ player_ts/prod/app.js
@@ -6,6 +6,33 @@ class Player {
         this.weight = weight;
         this.age = age;
     }
+    get nome() {
+        return this.name;
+    }
+    get altura() {
+        return this.height;
+    }
+    get peso() {
+        return this.weight;
+    }
+    get idade() {
+        return this.age;
+    }
 }
-let player1 = new Player('Lázaro', 1.70, 67, 18);
-console.log(player1);
+const player = new Player("John", 1.8, 70, 18);
+function addTr() {
+    var _a;
+    let nomeTd = document.createElement("td");
+    let alturaTd = document.createElement("td");
+    let pesoTd = document.createElement("td");
+    let idadeTd = document.createElement("td");
+    nomeTd.textContent = player.nome;
+    alturaTd.textContent = player.altura.toString();
+    pesoTd.textContent = player.peso.toString();
+    idadeTd.textContent = player.idade.toString();
+    let linha = document.createElement("tr");
+    linha.append(nomeTd, alturaTd, pesoTd, idadeTd);
+    (_a = document.querySelector(".tabela")) === null || _a === void 0 ? void 0 : _a.appendChild(linha);
+}
+const button = document.querySelector("#button-add");
+button === null || button === void 0 ? void 0 : button.addEventListener("click", addTr);
