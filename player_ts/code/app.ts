bbde2c3a68022804fcbcd66de63d7aa643e9303d
= <=>
2023-03-13 20:21:00 -0300

    feat: adding players to the table by the form

--- player_ts/code/app.ts
+++ player_ts/code/app.ts
@@ -1,49 +1,67 @@
-class Player {
-  constructor(
-    public name: string,
-    public height: number,
-    public weight: number,
-    public age: number
-  ) {}
-
-  get nome() {
-    return this.name;
-  }
-
-  get altura() {
-    return this.height;
-  }
-
-  get peso() {
-    return this.weight;
-  }
-
-  get idade() {
-    return this.age;
-  }
-
+interface IPlayer {
+  name: string;
+  height: number;
+  weight: number;
+  age: number;
 }
-const player = new Player("John", 1.8, 70, 18);
+// const player1: Player = {
+//   name: 'John',
+//   height: 180,
+//   weight: 75,
+//   age: 19
+// }
 
-function addTr() {
-  let nomeTd = document.createElement("td");
-  let alturaTd = document.createElement("td");
-  let pesoTd = document.createElement("td");
-  let idadeTd = document.createElement("td");
+function obtemPlayerForm(form: any) {
+  let player: IPlayer = {
+    name: form.nome.value,
+    height: form.altura.value,
+    weight: form.peso.value,
+    age: form.idade.value,
+  };
 
+  return player;
+}
+
+function addTd(dado: string, classe: string) {
+  const td = document.createElement("td");
+
+  td.textContent = dado;
+  td.classList.add(classe);
   
-  nomeTd.textContent = player.nome;
-  alturaTd.textContent = player.altura.toString();
-  pesoTd.textContent = player.peso.toString();
-  idadeTd.textContent = player.idade.toString();
+  return td;
+}
+
+function addTr(jogador: IPlayer) {
+  
+  const nomeTd = addTd(jogador.name, "info-nome");
+  const alturaTd = addTd(jogador.height.toString(), "info-altura");
+  const pesoTd = addTd(jogador.weight.toString(), "info-peso");
+  const idadeTd = addTd(jogador.age.toString(), "info-idade");
 
   let linha = document.createElement("tr");
+  linha.classList.add("jogador");
   linha.append(nomeTd, alturaTd, pesoTd, idadeTd);
 
-  document.querySelector(".tabela")?.appendChild(linha);
+  return linha;
+}
+
+function addPLayerTabela(jogador: IPlayer) {
+  let jogadorTr = addTr(jogador);
+  let tabela = document.querySelector(".tabela-jogador");
+  tabela?.appendChild(jogadorTr);
+  
+  return jogadorTr;
 }
 
 const button = document.querySelector("#button-add");
-button?.addEventListener("click", addTr);
+button?.addEventListener("click", (event) => {
+  event.preventDefault();
 
+  let form:any = document.querySelector("#form-adiciona");
 
+  let player = obtemPlayerForm(form);
+
+  addPLayerTabela(player);
+
+  form.reset()
+});
--- player_ts/prod/app.js
+++ player_ts/prod/app.js
@@ -1,38 +1,46 @@
 "use strict";
-class Player {
-    constructor(name, height, weight, age) {
-        this.name = name;
-        this.height = height;
-        this.weight = weight;
-        this.age = age;
-    }
-    get nome() {
-        return this.name;
-    }
-    get altura() {
-        return this.height;
-    }
-    get peso() {
-        return this.weight;
-    }
-    get idade() {
-        return this.age;
-    }
+// const player1: Player = {
+//   name: 'John',
+//   height: 180,
+//   weight: 75,
+//   age: 19
+// }
+function obtemPlayerForm(form) {
+    let player = {
+        name: form.nome.value,
+        height: form.altura.value,
+        weight: form.peso.value,
+        age: form.idade.value,
+    };
+    return player;
 }
-const player = new Player("John", 1.8, 70, 18);
-function addTr() {
-    var _a;
-    let nomeTd = document.createElement("td");
-    let alturaTd = document.createElement("td");
-    let pesoTd = document.createElement("td");
-    let idadeTd = document.createElement("td");
-    nomeTd.textContent = player.nome;
-    alturaTd.textContent = player.altura.toString();
-    pesoTd.textContent = player.peso.toString();
-    idadeTd.textContent = player.idade.toString();
+function addTd(dado, classe) {
+    const td = document.createElement("td");
+    td.textContent = dado;
+    td.classList.add(classe);
+    return td;
+}
+function addTr(jogador) {
+    const nomeTd = addTd(jogador.name, "info-nome");
+    const alturaTd = addTd(jogador.height.toString(), "info-altura");
+    const pesoTd = addTd(jogador.weight.toString(), "info-peso");
+    const idadeTd = addTd(jogador.age.toString(), "info-idade");
     let linha = document.createElement("tr");
+    linha.classList.add("jogador");
     linha.append(nomeTd, alturaTd, pesoTd, idadeTd);
-    (_a = document.querySelector(".tabela")) === null || _a === void 0 ? void 0 : _a.appendChild(linha);
+    return linha;
+}
+function addPLayerTabela(jogador) {
+    let jogadorTr = addTr(jogador);
+    let tabela = document.querySelector(".tabela-jogador");
+    tabela === null || tabela === void 0 ? void 0 : tabela.appendChild(jogadorTr);
+    return jogadorTr;
 }
 const button = document.querySelector("#button-add");
-button === null || button === void 0 ? void 0 : button.addEventListener("click", addTr);
+button === null || button === void 0 ? void 0 : button.addEventListener("click", (event) => {
+    event.preventDefault();
+    let form = document.querySelector("#form-adiciona");
+    let player = obtemPlayerForm(form);
+    addPLayerTabela(player);
+    form.reset();
+});
